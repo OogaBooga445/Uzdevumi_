b6059efcd5949238367cc3a17e49bee94cbfd696
OogaBooga445 <117642804+OogaBooga445@users.noreply.github.com>
2025-02-25 07:41:59 +0000

    kkas

---
+++ script.js
@@ -0,0 +1,25 @@
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        });
+        });
+        function toggleDarkMode() {
+            document.body.classList.toggle('dark-mode');
+        }
